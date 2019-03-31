a17db63a68fab296abc4f4bbedc448ab11ef6f11
Diego Garber <diego_garber@yahoo.com.ar>
2019-03-31 11:11:48 -0500

    added information to make it usable in an easy way. Added libraries folder

--- NeatTest/NeatTest.Console/Program.cs
+++ NeatTest/NeatTest.Console/Program.cs
@@ -14,6 +14,8 @@ namespace NeatTest.ConsoleApp
 
         static void Main(string[] args)
         {
+            bool train = true;
+
             GameExperiment experiment = new GameExperiment();
 
             // Load config XML.
@@ -21,56 +23,66 @@ namespace NeatTest.ConsoleApp
             xmlConfig.Load("expconfig.xml");
             experiment.Initialize("TicTacToe", xmlConfig.DocumentElement);
 
-
-            #region Play
-            NeatGenome genome = null;
-
-            // Try to load the genome from the XML document.
-            try
+            if (!train)
             {
-                using (XmlReader xr = XmlReader.Create(FILE))
-                    genome = NeatGenomeXmlIO.ReadCompleteGenomeList(xr, false, (NeatGenomeFactory)experiment.CreateGenomeFactory())[0];
+                #region Play
+                NeatGenome genome = null;
+
+                // Try to load the genome from the XML document.
+                try
+                {
+                    using (XmlReader xr = XmlReader.Create(FILE))
+                        genome = NeatGenomeXmlIO.ReadCompleteGenomeList(xr, false, (NeatGenomeFactory)experiment.CreateGenomeFactory())[0];
+                }
+                catch (Exception e1)
+                {
+                    return;
+                }
+
+                // Get a genome decoder that can convert genomes to phenomes.
+                var genomeDecoder = experiment.CreateGenomeDecoder();
+
+                // Decode the genome into a phenome (neural network).
+                var phenome = genomeDecoder.Decode(genome);
+
+                IPlayer p1 = new NeatBrain(phenome);
+                IPlayer p2 = new RandomBrain();
+
+                while (true)
+                {
+
+                    Game Game = new Game(p1, p2);
+                    Game.PlayUntilFinished();
+
+                    Draw(Game.Board);
+
+                    Console.WriteLine(Game.GetGameState().ToString());
+                    Console.ReadKey();
+                    Console.WriteLine();
+                }
+                #endregion Play
             }
-            catch (Exception e1)
+            else
             {
-                return;
+                #region Instructions
+                Console.WriteLine("This is the training example. If you want to APPLY it, modify the boolean 'train' to false.");
+                Console.WriteLine("Take into acocunt that training generates a file 'expconfig.xml' that you can load to play.");
+                #endregion
+
+
+                #region Train
+                NeatEvolutionAlgorithm<NeatGenome> _ea;
+
+                // Create evolution algorithm and attach update event.
+                _ea = experiment.CreateEvolutionAlgorithm();
+                _ea.UpdateEvent += new EventHandler(ea_UpdateEvent);
+
+                // Start algorithm (it will run on a background thread).
+                _ea.StartContinue();
+
+                Console.ReadLine();
+                #endregion Train
             }
-
-            // Get a genome decoder that can convert genomes to phenomes.
-            var genomeDecoder = experiment.CreateGenomeDecoder();
-
-            // Decode the genome into a phenome (neural network).
-            var phenome = genomeDecoder.Decode(genome);
-
-            IPlayer p1 = new NeatBrain(phenome);
-            IPlayer p2 = new RandomBrain();
-
-            while (true)
-            {
-
-                Game Game = new Game(p1, p2);
-                Game.PlayUntilFinished();
-
-                Draw(Game.Board);
-
-                Console.WriteLine(Game.GetGameState().ToString());
-                Console.ReadKey();
-                Console.WriteLine();
-            }
-            #endregion Play
-
-            #region Train
-            //NeatEvolutionAlgorithm<NeatGenome> _ea;
-
-            //// Create evolution algorithm and attach update event.
-            //_ea = experiment.CreateEvolutionAlgorithm();
-            //_ea.UpdateEvent += new EventHandler(ea_UpdateEvent);
-
-            //// Start algorithm (it will run on a background thread).
-            //_ea.StartContinue();
-
-            //Console.ReadLine();
-            #endregion Train
         }
 
         private static void ea_UpdateEvent(object sender, EventArgs e)
